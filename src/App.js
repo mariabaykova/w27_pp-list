6d19a27ed4c4fd1fb41f7880251a02a79a37ef1f
Maria Baykova <mnbaikova@gmail.com>
2022-07-11 00:14:02 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -18,6 +18,8 @@ function App() {
           Learn React
         </a>
       </header>
+      <main className="App-main">main</main>
+      <footer>footer</footer>
     </div>
   );
 }
---
+++ src/assets/components/PPCard.js
@@ -0,0 +1,3 @@
+import styles from "../styles/PPCard.scss";
+
+export default function PPCard() {}
