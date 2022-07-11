0742ed9f795bc5abe65947867333407a24e8253e
Maria Baykova <mnbaikova@gmail.com>
2022-07-11 09:02:18 +0500

    some polish again

--- src/assets/components/PPCard.js
+++ src/assets/components/PPCard.js
@@ -5,7 +5,7 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 // import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import styles from "../styles/PPCard.scss";
+// import styles from "../styles/PPCard.scss";
 
 const bull = (
   <Box
