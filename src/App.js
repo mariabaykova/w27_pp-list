09639a37a3f6a71ddd49fadcaefc9bff1901a2d7
Maria Baykova <mnbaikova@gmail.com>
2022-07-11 08:56:40 +0500

    test ver to try mui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
 import logo from "./logo.svg";
 import "./App.css";
+import PPCard from "./assets/components/PPCard";
+
+const ppList = [
+  {
+    ppId: 1,
+    ppName: "Безлимитный 300",
+    subscrFee: 300,
+    speed: 10,
+    trafficVolume: "не ограничен",
+    selected: false,
+  },
+  {
+    ppId: 2,
+    ppName: "Безлимитный 450",
+    subscrFee: 450,
+    speed: 50,
+    trafficVolume: "не ограничен",
+    selected: false,
+  },
+  {
+    ppId: 3,
+    ppName: "Безлимитный 550",
+    subscrFee: 550,
+    speed: 100,
+    trafficVolume: "не ограничен",
+    selected: true,
+  },
+  {
+    ppId: 4,
+    ppName: "Безлимитный 1000",
+    subscrFee: 1000,
+    speed: 200,
+    trafficVolume: "не ограничен",
+    selected: false,
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        It's just to show the example of conditional rendering
       </header>
-      <main className="App-main">main</main>
+      <main className="App-main">
+        {ppList &&
+          ppList.map(
+            ({ ppName, subscrFee, speed, trafficVolume, selected }) => (
+              <PPCard />
+            )
+          )}
+      </main>
       <footer>footer</footer>
     </div>
   );
